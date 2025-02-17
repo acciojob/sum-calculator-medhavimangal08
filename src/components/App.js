3398813b5b984e487d8e0127ad9a6147f9f15723
medhavimangal08 <medhavimangal43@gmail.com>
2025-02-18 00:06:30 +0530

    sumcalcreact

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  let [num, setNum] = useState(0);
+  let [sum, calcSum] = useState(0);
+  useEffect(() => {
+    calcSum(sum + num);
+  }, [num]);
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Sum Calculator</h1>
+      <input
+        type="number"
+        onChange={(e) => {
+          setNum(e.target.value);
+        }}
+      ></input>
+      <p>Sum: {sum}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
